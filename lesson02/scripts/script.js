a0831b81b199b289b1410ea6a0c27b34043acf2e
trukkerweeler <ken22008@byui.edu>
2022-10-16 16:34:24 -0600

    w02 design-principles

---
+++ lesson02/scripts/script.js
@@ -0,0 +1,14 @@
+const year1 = document.querySelector("#year1");
+try {
+    const options = {
+        year: "numeric"
+    };
+    year1.textContent = new Date().toLocaleDateString("en-UK", options);
+}
+catch (e) {
+    alert("Error with code or your browser does not support Locale (year)");
+}
+
+//get the last modified date
+let datelastmodified = document.lastModified;
+document.getElementById("portallastmodified").innerHTML = `Last modified: ${datelastmodified}`;
