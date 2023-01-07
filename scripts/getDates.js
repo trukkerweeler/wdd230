01390e5d1a1e4d585b5a614a718df4d8c42a7a30
trukkerweeler <ken22008@byui.edu>
2023-01-07 07:01:11 -0700

    delete-all

--- scripts/getDates.js
+++
@@ -1,6 +0,0 @@
-//get the copyright year
-document.querySelector('#©').textContent = new Date(Date.now()).getFullYear();
-
-//get the last modified date
-let lastModified = document.lastModified;
-document.querySelector('#lastModified').innerHTML = `Last modified: ${lastModified}`;
