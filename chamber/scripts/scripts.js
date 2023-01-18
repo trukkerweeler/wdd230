eef1219eb3315472b644b4f6c0cb67617c718222
trukkerweeler <ken22008@byui.edu>
2023-01-18 07:18:35 -0700

    02_chamber_closer

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -1,10 +1,13 @@
 // Store the selected elements that we are going to use. 
-const mainnav = document.querySelector('.navigation')
-const hambutton = document.querySelector('#menu');
+const mybtn = document.querySelector('header button')
+const mynav = document.querySelector('nav');
 
 // Add a simple arrow function that listens for the <li> hamburger button click event.
 // When clicked, the <ul class="navigation">'s class list toggle'
-hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+mybtn.addEventListener('click', () => {
+    mybtn.classList.toggle('open')
+    mynav.classList.toggle('open')
+});
 
 // What does toggle mean? Instead of add and remove, toggle means add the class name (the parameter, which in this case is named 'responsive') if it does not currently exist, and remove the 
 
